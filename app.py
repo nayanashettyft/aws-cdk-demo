e7047e0cae41675b0bdcfd497dbfdd940b3e1d13
nayana.shetty <nayana.shetty@ft.com>
2019-07-31 17:35:26 +0100

    added sqs and lambda

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 from aws_cdk import core
+import aws_cdk.aws_sqs as sqs
 
 from hello_cdk.hello_cdk_stack import HelloCdkStack
 
+stack_tags = {"systemCode": "id-rnd", 
+    "teamDL": "ent.svcs.fs.id@ft.com",
+    "environment": "d"}
 
 app = core.App()
-HelloCdkStack(app, "hello-cdk")
+HelloCdkStack(app, "hello-cdk", tags=stack_tags)
 
 app.synth()
--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,9 +1,27 @@
 from aws_cdk import core
+import aws_cdk.aws_sqs as _sqs
+import aws_cdk.aws_lambda as _lambda
+import aws_cdk.aws_lambda_event_sources as event_sources
 
 
 class HelloCdkStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+
+        queue = _sqs.Queue(
+                    self,
+                    'HelloQueue'
+        )
+
+        my_lambda = _lambda.Function(
+                        self,
+                        'HelloFunction', 
+                        runtime=_lambda.Runtime.PYTHON_3_7,
+                        code=_lambda.Code.asset('lambda'),
+                        handler='hello.handler'
+        )
+
+        my_lambda.add_event_source(event_sources.SqsEventSource(queue))
 
         # The code that defines your stack goes here
