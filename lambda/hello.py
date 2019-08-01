7d9c26a8f0f9c2f586f41d9b7c98c87c994d3d6d
nayana.shetty <nayana.shetty@ft.com>
2019-08-01 15:25:34 +0100

    updated the lambda function for better output

--- lambda/hello.py
+++ lambda/hello.py
@@ -3,10 +3,12 @@ import json
 
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
+    jevent = json.dumps(event)
+    print(jevent['Records'])
     return {
         'statusCode': 200,
         'headers': {
             'Content-Type': 'text/plain'
         },
-        'body': 'Hello, CDK! You have hit {}\n'.format(event['path'])
+        'body': 'Hello, CDK! You sent us a message'
     }
